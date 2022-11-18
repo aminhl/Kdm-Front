e7c5b0566eb99f0a032ad4b2b0311c89a6a42bc9
aminhl <amin.hlel@esprit.tn>
2022-11-18 14:58:19 +0100

    integrating_main_components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './components/admin/header/header.component';
+import { SidebarComponent } from './components/admin/sidebar/sidebar.component';
+import { FooterComponent } from './components/admin/footer/footer.component';
+import { ErrorComponent } from './pages/error/error.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes : Routes = [
+  {path: '**', component: ErrorComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    ErrorComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
