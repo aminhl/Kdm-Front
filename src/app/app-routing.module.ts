953cf5a91bf130bd8cc92d8513b2605a891fcbfd
aminhl <amin.hlel@esprit.tn>
2022-12-08 16:21:27 +0100

    implimenting_add_and_get_club

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,35 +8,73 @@ import { ProfileComponent } from './pages/admin/profile/profile.component';
 import { ErrorComponent } from './pages/admin/error/error.component';
 import { ContratComponent } from './pages/admin/contrat/contrat.component';
 import { AddContratComponent } from './pages/admin/contrat/add-contrat/add-contrat.component';
-import { EquipeComponent } from "./pages/admin/equipe/equipe.component";
-import { AddEquipeComponent } from "./pages/admin/equipe/add-equipe/add-equipe.component";
+import { EquipeComponent } from './pages/admin/equipe/equipe.component';
+import { AddEquipeComponent } from './pages/admin/equipe/add-equipe/add-equipe.component';
 import { EtudiantComponent } from './pages/admin/etudiant/etudiant.component';
 import { UniversiteComponent } from './pages/admin/universite/universite.component';
-import { LoggedInGuard } from "./pages/LoggedInGuard";
-import {DashboardComponent} from "./pages/admin/dashboard/dashboard.component";
-
-
+import { LoggedInGuard } from './pages/LoggedInGuard';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { ClubComponent } from './pages/admin/club/club.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'contrat', component: ContratComponent, canActivate: [LoggedInGuard] },
-  { path: 'addcontrat', component: AddContratComponent, canActivate: [LoggedInGuard] },
-  { path: 'equipe', component: EquipeComponent, canActivate: [LoggedInGuard], },
-  { path: 'addequipe', component: AddEquipeComponent, canActivate: [LoggedInGuard] },
-  { path: 'etudiant', component: EtudiantComponent, canActivate: [LoggedInGuard] },
-  { path: 'login', component: LoginComponent, },
-  { path: 'contact', component: ContactComponent, canActivate: [LoggedInGuard] },
-  { path: 'profile', component: ProfileComponent, canActivate: [LoggedInGuard] },
+  {
+    path: 'contrat',
+    component: ContratComponent,
+    canActivate: [LoggedInGuard],
+  },
+  {
+    path: 'addcontrat',
+    component: AddContratComponent,
+    canActivate: [LoggedInGuard],
+  },
+  { path: 'equipe', component: EquipeComponent, canActivate: [LoggedInGuard] },
+  {
+    path: 'addequipe',
+    component: AddEquipeComponent,
+    canActivate: [LoggedInGuard],
+  },
+  {
+    path: 'etudiant',
+    component: EtudiantComponent,
+    canActivate: [LoggedInGuard],
+  },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'contact',
+    component: ContactComponent,
+    canActivate: [LoggedInGuard],
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [LoggedInGuard],
+  },
   { path: 'register', component: RegisterComponent },
-  { path: 'departement', loadChildren: () => import('./pages/admin/departement/departement.module').then(m => m.DepartementsModule), canActivate: [LoggedInGuard] },
-  { path: 'universite', component: UniversiteComponent,canActivate:[LoggedInGuard] },
-  {path: 'dashboard', component: DashboardComponent, canActivate: [LoggedInGuard]},
+  {
+    path: 'departement',
+    loadChildren: () =>
+      import('./pages/admin/departement/departement.module').then(
+        (m) => m.DepartementsModule
+      ),
+    canActivate: [LoggedInGuard],
+  },
+  {
+    path: 'universite',
+    component: UniversiteComponent,
+    canActivate: [LoggedInGuard],
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [LoggedInGuard],
+  },
+  { path: 'club', component: ClubComponent, canActivate: [LoggedInGuard] },
   { path: '**', component: ErrorComponent },
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
