8ccbce71c902991a0fd817cdc9f536d0c450ea99
NaderAyed <nader.ayed@esprit.tn>
2022-12-11 22:51:55 +0100

    fixing logging out after adding or reloading page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LoggedInGuard} from "./pages/LoggedInGuard";
+import {LoginComponent} from "./pages/admin/login/login.component";
+import {AuthService} from "./_services/auth.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'ngKaddem';
-  currentUsername="";
+  currentUsername=window.sessionStorage.getItem("username");
 
   addUsername(usename:string){
-    this.currentUsername=usename;
+    window.sessionStorage.setItem("username",usename);
+    this.currentUsername=window.sessionStorage.getItem("username");
   }
+
+
+
 }
