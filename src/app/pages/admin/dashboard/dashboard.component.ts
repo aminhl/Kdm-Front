e56cb526acf873260149a42447fd2c0a640e908b
KhairouneAymen <aymen.khairoune@esprit.tn>
2022-12-12 21:10:48 +0100

    dashboard univ stats

--- src/app/pages/admin/dashboard/dashboard.component.ts
+++ src/app/pages/admin/dashboard/dashboard.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ApiService } from 'src/app/core/services/admin/api.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  universites!: any
+  constructor(private apiService: ApiService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {    
+    this.apiService.refreshNeeded.subscribe(
+      () => {
+    this.CountUniversites();
+  }
+  )
+  this.CountUniversites();
   }
 
+  CountUniversites(){
+    this.apiService
+      .get('countUniversites')
+      .subscribe((universites) => (this.universites = universites));
+  }
 }
