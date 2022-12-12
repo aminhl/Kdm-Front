4ae11f0fbc683afe01b3210ded895e74840e2dfe
aminhl <amin.hlel@esprit.tn>
2022-12-12 22:45:59 +0100

    fixing_more_bugs

--- src/app/core/services/admin/api.service.ts
+++ src/app/core/services/admin/api.service.ts
@@ -1,32 +1,29 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
-import { environment as env, environment } from '../../../../environments/environment';
-import {map, Observable, Subject, tap} from "rxjs";
-import {IArchivePercentType} from "../../models/ArchivePercentType";
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import {
+  environment as env,
+  environment,
+} from '../../../../environments/environment';
+import { map, Observable, Subject, tap } from 'rxjs';
+import { IArchivePercentType } from '../../models/ArchivePercentType';
 
 @Injectable({
   providedIn: 'root',
 })
-
 export class ApiService {
+  constructor(private httpClient: HttpClient) {}
+  private _refreshNeeded = new Subject<void>();
 
-
-  constructor(private httpClient: HttpClient) {
-  }
-private _refreshNeeded=new Subject<void>();
-
-  get refreshNeeded()
-  {
+  get refreshNeeded() {
     return this._refreshNeeded;
   }
   get(target: string) {
     return this.httpClient.get(env.apiRoot + target);
   }
 
-
   add(target: string, requestBody: Object) {
     return this.httpClient.post(env.apiRoot + target, requestBody).pipe(
-      tap(()=>{
+      tap(() => {
         this._refreshNeeded.next();
       })
     );
@@ -34,21 +31,20 @@ private _refreshNeeded=new Subject<void>();
 
   delete(target: string, elementId: number) {
     return this.httpClient.delete(env.apiRoot + target + '/' + elementId).pipe(
-      tap(()=>{
+      tap(() => {
         this._refreshNeeded.next();
       })
     );
   }
 
   update(target: string, elementId: number, requestBody: Object) {
-    return this.httpClient.put(
-      env.apiRoot + target + '/' + elementId,
-      requestBody
-    ).pipe(
-      tap(()=>{
-        this._refreshNeeded.next();
-      })
-    );
+    return this.httpClient
+      .put(env.apiRoot + target + '/' + elementId, requestBody)
+      .pipe(
+        tap(() => {
+          this._refreshNeeded.next();
+        })
+      );
   }
 
   // ContratArchiveStats
@@ -60,82 +56,121 @@ private _refreshNeeded=new Subject<void>();
       .pipe(map((d: Array<IArchivePercentType>) => d));
   }
 
-
   login(username, password): Observable<any> {
     const body = new HttpParams()
       .set('username', username)
       .set('password', password);
-    return this.httpClient.post("http://localhost:8080/login",
+    return this.httpClient.post(
+      'http://localhost:8080/login',
       body.toString(),
       {
-        headers: new HttpHeaders()
-          .set('Content-Type', 'application/x-www-form-urlencoded')
-
+        headers: new HttpHeaders().set(
+          'Content-Type',
+          'application/x-www-form-urlencoded'
+        ),
       }
     );
   }
 
-
   //pdf
-  exportPDF(path: string):Observable<Blob> {
-    return this.httpClient.get(environment.apiRoot + path, {responseType: 'blob'});
+  exportPDF(path: string): Observable<Blob> {
+    return this.httpClient.get(environment.apiRoot + path, {
+      responseType: 'blob',
+    });
   }
 
-
-
-  register(target: string, requestBody: Object){
+  register(target: string, requestBody: Object) {
     return this.httpClient.post(env.apiRoot + target, requestBody);
   }
 
-
   // get anything by id
-  getbyID(target: string,elementId: number) {
+  getbyID(target: string, elementId: number) {
     return this.httpClient.get(env.apiRoot + target + '/' + elementId);
   }
-  getbyName(target: string,elementId: string) {
+  getbyName(target: string, elementId: string) {
     return this.httpClient.get(env.apiRoot + target + '/' + elementId);
   }
   // assignEtudiantToDepartement
-  assignEtudiantToDepartement(target : string, elementId1: number, elementId2: number)
-  {
-    return this.httpClient.put(env.apiRoot +target +'/'+elementId1+ '/' +elementId2,null).pipe(
-      tap(()=>{
-        this._refreshNeeded.next();
-      })
+  assignEtudiantToDepartement(
+    target: string,
+    elementId1: number,
+    elementId2: number
+  ) {
+    return this.httpClient
+      .put(env.apiRoot + target + '/' + elementId1 + '/' + elementId2, null)
+      .pipe(
+        tap(() => {
+          this._refreshNeeded.next();
+        })
+      );
+  }
+
+  addAndAssignEtudiantToEquipeAndContract(
+    target: string,
+    elementId1: number,
+    elementId2: number,
+    requestBody: Object
+  ) {
+    return this.httpClient
+      .put(
+        env.apiRoot + target + '/' + elementId1 + '/' + elementId2,
+        requestBody
+      )
+      .pipe(
+        tap(() => {
+          this._refreshNeeded.next();
+        })
+      );
+  }
+  findContratBySpecialiteAndDateDebutContratAndDateFinContrat(
+    target: string,
+    elementId1: string,
+    elementId2: string,
+    elementId3: string,
+    elementId4: string
+  ) {
+    return this.httpClient
+      .get(
+        env.apiRoot +
+          target +
+          '/' +
+          elementId1 +
+          '/' +
+          elementId2 +
+          '/' +
+          elementId3 +
+          '/' +
+          elementId4
+      )
+      .pipe(
+        tap(() => {
+          this._refreshNeeded.next();
+        })
+      );
+  }
+
+  assignProfessorToDepartement(
+    target: string,
+    elementId1: number,
+    elementId2: number
+  ) {
+    return this.httpClient
+      .put(env.apiRoot + target + '/' + elementId1 + '/' + elementId2, null)
+      .pipe(
+        tap(() => {
+          this._refreshNeeded.next();
+        })
+      );
+  }
+
+  assignEquipeToDetailEquipe(
+    target: string,
+    element1: string,
+    element2: string
+  ) {
+    return this.httpClient.put(
+      env.apiRoot + target + '/' + element1 + '/' + element2,
+      null
     );
   }
-
-  addAndAssignEtudiantToEquipeAndContract(target : string, elementId1: number, elementId2: number,requestBody: Object) {
-    return this.httpClient.put(env.apiRoot+target +'/'+elementId1+ '/' +elementId2,requestBody ).pipe(
-      tap(()=>{
-        this._refreshNeeded.next();
-      })
-    );
-  }
-  findContratBySpecialiteAndDateDebutContratAndDateFinContrat(target : string, elementId1: string, elementId2: string,elementId3: string,elementId4: string) {
-    return this.httpClient.get(env.apiRoot+target +'/'+elementId1+ '/' +elementId2+ '/' +elementId3+ '/' +elementId4 ).pipe(
-      tap(()=>{
-        this._refreshNeeded.next();
-      })
-    );
-  }
-
-   assignProfessorToDepartement(target : string, elementId1: number, elementId2: number)
-  {
-    return this.httpClient.put(env.apiRoot +target +'/'+elementId1+ '/' +elementId2,null).pipe(
-      tap(()=>{
-        this._refreshNeeded.next();
-      })
-    );
-  }
-
-  assignEquipeToDetailEquipe(target : string, element1: string, element2: string) {
-    return this.httpClient.put(env.apiRoot+target +'/'+element1+ '/' +element2,null )
-  }
-
-  assignEquipeToDetailEquipe(target : string, element1: string, element2: string) {
-    return this.httpClient.put(env.apiRoot+target +'/'+element1+ '/' +element2,null )
-  }
-
-
 }
